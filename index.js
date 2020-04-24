cb87d0f6cc4aa473c2a2188e1244b28944d0ad0e
eldroan <leandroamarillo95@gmail.com>
2020-04-24 01:21:10 -0300

    Login contra el sysacad

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 var auth = require("basic-auth");
+var rp = require("request-promise");
+var cheerio = require("cheerio");
 
 app.use(cors());
 
@@ -12,22 +14,96 @@ const checkBasicAuth = function (req, res, next) {
     next();
   } else {
     //No viene con token
-    res
-      .status(401)
-      .json({
-        status: 401,
-        message: "No me mandaste ningun header de basic auth :C",
-      });
+    res.status(403).json({
+      status: 403,
+      message: "No me mandaste ningun header de basic auth :C",
+    });
   }
 };
 
 app.use(checkBasicAuth);
 
+//HelloWord
 app.get("/hello", function (req, res, next) {
   res.json({
     msg: "Recibo CORS!",
-    auth: auth(req) ?? "No me mandaste ningun header de basic auth :C",
+    auth: auth(req),
   });
+});
+
+app.get("/login", async function (req, res, next) {
+  const { name: legajo, pass: password } = auth(req);
+  if (legajo && password) {
+    const params = new URLSearchParams();
+    params.append("collection", "yes");
+    params.append("legajo", legajo);
+    params.append("password", password);
+
+    try {
+      const html = await rp.post(
+        "https://sysacad.frsf.utn.edu.ar/SysAcad/menuAlumno.asp",
+        {
+          form: {
+            collection: "yes",
+            legajo: legajo,
+            password: password,
+          },
+        }
+      );
+
+      if (html) {
+        //Si tengo respuesta y no es vacia
+        const $ = cheerio.load(html);
+        const error = $(".textoError");
+        if (error.length == 0) {
+          //Login exitoso
+          const nombreAlumno = $(".tituloTabla").text().trim();
+          const opciones = $("li > a")
+            .map(function (idx, element) {
+              return {
+                titulo: element.firstChild.nodeValue,
+                ruta: element.attribs.href,
+              }; //El dia que el sysacad cambie esto va a reventar
+            })
+            .toArray();
+
+          //TODO: Guardarse la cookie y extraer toda esa funcionalidad para reusarla en los demas endpoints
+          res.status(200).json({
+            status: 200,
+            message: "Ok",
+            response: {
+              alumno: nombreAlumno,
+              endpoints: opciones,
+            },
+          });
+        } else {
+          console.log(error);
+          res.status(401).json({
+            status: 401,
+            message: error.text().split("�").join(""), //Borramos los � que a veces devuelve el sysacad
+          });
+        }
+      } else {
+        //El sysacad respondio vacio
+        res.status(500).json({
+          status: 500,
+          message: "El sysacad dio una respuesta invalida.",
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        status: 500,
+        message: "El sysacad no pudo responder.",
+      });
+    }
+  } else {
+    res.status(401).json({
+      status: 401,
+      message:
+        "No se puede extraer el usuario o la contraseña de tu request o estas fueron vacias.",
+    });
+  }
 });
 
 app.listen(80, function () {
